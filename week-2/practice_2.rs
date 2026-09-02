36d5b42db058e4a0f3e3fc7ad39a73c708ba7c1b
vmamudu <eshialema.mamudu@pau.edu.ng>
2026-09-02 16:52:32 +0100

    practice tweo

---
+++ week-2/practice_2.rs
@@ -0,0 +1,11 @@
+fn main () {
+
+	println! ();
+
+	println!("hello :3"); //prints hello i hope
+
+	println!("format {} arguments", "a few"); //prints format a few arguments
+
+	println!("my name is {}. I am a student of cos101 SEN!", "valerie");
+
+}
