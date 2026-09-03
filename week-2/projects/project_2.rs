a87a07b23c2bae36fddc4ce1de5d7a9454101d2b
vmamudu <eshialema.mamudu@pau.edu.ng>
2026-09-03 17:06:41 +0100

    sales report

---
+++ week-2/projects/project_2.rs
@@ -0,0 +1,12 @@
+fn main () {
+	let t = 450_000 * 2;
+	let m = 1_500_000 * 1;
+	let hp = 750_000 * 3;
+let d = 2_850_000 * 3;
+let a = 250_000 * 1;
+let sum = t + m + hp + d + a;
+println!("sum of sales products is {}", sum);
+let totalp = 2 + 3 + 3 + 1 + 1;
+let av = sum / totalp;
+println! ("average sales of products is {}", av);
+	}
