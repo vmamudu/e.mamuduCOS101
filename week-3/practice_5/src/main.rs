f568f5603ba8825390171b59766d87464300a776
vmamudu <eshialema.mamudu@pau.edu.ng>
2026-09-13 16:10:14 +0100

    characters and strings

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+let special_character = "@"; //default
+let alphabet:char = 'B';
+let surname = "Bodija";
+let first_name = "Gbayike";
+let middle_name = "Tamilore";
+
+println!(" ");
+println!("special character: {}",special_character);
+println!("Alphabet: {} ",alphabet );
+println!("Name: {} {} {} ",surname, middle_name, first_name );
+
+}
