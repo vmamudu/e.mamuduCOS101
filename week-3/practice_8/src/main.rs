13458549e7bd0feb3294488d2f4e54251f1797cd
vmamudu <eshialema.mamudu@pau.edu.ng>
2026-09-13 16:21:56 +0100

    rust is immutable

---
+++ week-3/practice_8/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let fees = 25_000;
+    println!("fees is {}",fees );
+// by default, rust variables are immutable and cannot be freely changed.
+    let Fees = 35_000; //change in variable to allow code to run
+    println!("fees changed is {}",Fees );
+}
