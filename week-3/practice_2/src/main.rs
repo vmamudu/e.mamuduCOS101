c31e8169092f008f071b0e54f8a20a3851638860
vmamudu <eshialema.mamudu@pau.edu.ng>
2026-09-13 15:50:55 +0100

    types of integers

---
+++ week-3/practice_2/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+  let result = 10; //i32 by default
+  let age:u32 = 20;
+  let sum:i32 = 5 - 15;
+
+  println!("result value is {}", result);
+  println!("age is {}", age);
+  println!("sum is {}",sum);
+  
+}
