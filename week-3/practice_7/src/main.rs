efd870e610f44079a266d71a11f5ae9e9f7ee7bd
vmamudu <eshialema.mamudu@pau.edu.ng>
2026-09-13 16:16:51 +0100

    number seperator

---
+++ week-3/practice_7/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+   let float_with_seperator = 11_000.555_001; //seperators are purely aesthetic and do not affect your code in anyway
+   println!("float value {}",float_with_seperator );
+   let int_with_seperator = 50_000;
+   println!("int value {}",int_with_seperator );
+}
