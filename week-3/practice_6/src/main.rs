6d413c7de0c1661fea3561bba21c79eeb3d93e65
vmamudu <eshialema.mamudu@pau.edu.ng>
2026-09-13 16:14:09 +0100

    type casting

---
+++ week-3/practice_6/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+   let interest:f32 = 8.0; //automatic typcasting is not allowed in rust, therefore we must manually assign accurate data types in order for the code to run
+   println!("interest is {}",interest );
+}
