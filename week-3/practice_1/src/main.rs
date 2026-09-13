ba63a06a5fec047fee109727f1081020b29b1c44
vmamudu <eshialema.mamudu@pau.edu.ng>
2026-09-13 15:42:02 +0100

    different data types

---
+++ week-3/practice_1/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let school_string = "school of Science and Technology"; // string type :3
+    let rating_float = 4.9; // float type!
+    let is_growing_boolean = true; //boolean type
+    let department_integer = 3; //integer type
+
+    println!("my school name is: {}", school_string);
+    println!("my school rating on 5 is: {}", rating_float);
+    println!("school is growing: {}", is_growing_boolean);
+    println!("Number of departments :{ }", department_integer);
+}
