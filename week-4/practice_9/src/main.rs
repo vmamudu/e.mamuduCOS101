bf1ec7a2ed0d7bd63942fe35a3c2983795801121
vmamudu <eshialema.mamudu@pau.edu.ng>
2026-09-17 17:58:36 +0100

    loop using continue function

---
+++ week-4/practice_9/src/main.rs
@@ -0,0 +1,15 @@
+//continue function
+
+fn main() {
+ let mut count = 0;
+
+ for num in 1..21{
+    if num > 10{
+        println!("{:?}",num);
+        continue;
+    }
+    count+=1;
+ }
+ println!("The count of values greater than 10 (between 1 and 20) is: {}",count);
+ //outputs 10
+}
